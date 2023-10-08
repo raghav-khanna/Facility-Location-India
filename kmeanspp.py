7659de07fcdf647f6429cde798d9c3dbf5cf8fc9
Raghav Khanna <raghav.29.khanna@gmail.com>
2023-10-08 22:00:43 +0530

    Add Final CSV and add comments in the code

--- kmeanspp.py
+++ kmeanspp.py
@@ -5,7 +5,7 @@ from pyclustering.cluster.center_initializer import kmeans_plusplus_initializer
 from pyclustering.utils.metric import distance_metric, type_metric
 from math import cos, sin, asin, sqrt
 
-
+# Function for defining the custom distance metric (Haversine distance, in this case)
 def haversine(point1, point2):
     lat1, lon1 = point1[0], point1[1]
     lat2, lon2 = point2[0], point2[1]
@@ -16,37 +16,56 @@ def haversine(point1, point2):
     r = 6371  # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
     return c * r
 
-
+# The function for performing kmeans using the pyclustering library
 def perform_kmeans(data, no_of_facilities):
+    # Get initial centers using kmeans++ initializer
     initial_centers = kmeans_plusplus_initializer(data, no_of_facilities).initialize()
+    print(initial_centers)
+
+    # Create instance of K-Means algorithm with custom distance metric
     haversine_distance = distance_metric(type_metric.USER_DEFINED, func=haversine)
-    # print(initial_centers)
     kmeans_instance = kmeans(data, initial_centers, metric=haversine_distance)
 
+    # Are these final centers different from the initial centers? -> Most probably, NO
     final_centers = kmeans_instance.get_centers()
-    # print(); print(final_centers)
+    print(); print(final_centers)
+
+    # CHECK if this is NECESSARY? -> Most probably, yes
     # kmeans_instance.process()
+
+    # Get the final clusters
     # clusters = kmeans_instance.get_clusters()
+
+    # visualise using the pyclustering visualiser function
     # plt = kmeans_visualizer.show_clusters(data, clusters, final_centers, initial_centers=initial_centers)
+
+    # CHECK if this function produces an animation?!
     # plt = kmeans_visualizer.animate_cluster_allocation(data, clusters, observer...)
+
     return final_centers
 
 
 def main():
+    # Establish connection with the database
     db_details = dotenv_values('.env')
     conn = psycopg2.connect(dbname=db_details['DB_NAME'], user=db_details['DB_USER'], password=db_details['DB_PASS'], host=db_details['DB_HOST'])
     cur = conn.cursor()
     try:
+        # Retrieve the data from the database
         cur.execute("SELECT pop_density, latitude, longitude FROM districts;")
         data = cur.fetchall()
         coordinates = []
+        # Iterate through the whole data and create an array of pairs (latitude and longitude for each district)
         for row in data:
             for num in range(0, row[0]):
                 coordinates.append([float(row[1]), float(row[2])])
 
         try:
+            # Perform kmeans++ on the data
             final_cluster = perform_kmeans(coordinates, int(db_details['NO_OF_FACILITIES']))
             # print(final_cluster)
+
+            # Get the final district names and states
             chosen_districts = []
             for point in final_cluster:
                 cur.execute("SELECT name, state FROM districts WHERE latitude = (%s) AND longitude = (%s);", (point[0], point[1]))
