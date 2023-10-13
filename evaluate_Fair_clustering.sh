3bc279134addc4bc803bfe0edf74f343b1770de8
Raghav Khanna <raghav.29.khanna@gmail.com>
2023-10-13 20:46:10 +0530

    Initial Commit Ziko_etal

---
+++ evaluate_Fair_clustering.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+c_v_l=False # Set true for clusters vs lambda figures in Synthetic or Synthetic-unequal dataset
+f_v_E=False # Set true to check the fairness error vs Discrete clustering energy plots in a lambda range. Note that,
+          # In this case also set --lmd_tune to True to have the default range.
+conv=False # Set true to see if the algorithm converges.
+lmd_tune=False
+
+dataset=Synthetic-unequal
+cluster_option=ncut
+lmd=10
+py test_fair_clustering.py -d $dataset \
+                             --cluster_option $cluster_option \
+                             --lmbda-tune $lmd_tune \
+                             --lmbda $lmd \
+                             --L 1.0
+                            #  --plot_option_clusters_vs_lambda $c_v_l \
+                            #  --plot_option_fairness_vs_clusterE $f_v_E \
+                            #  --plot_option_convergence $conv
+
+#dataset=Bank
+#cluster_option=ncut
+#lmd=40
+#python test_fair_clustering.py -d $dataset \
+#                             --cluster_option $cluster_option \
+#                             --lmbda-tune $lmd_tune \
+#                             --lmbda $lmd \
+#                             --L 1.0 \
+#                             --plot_option_clusters_vs_lambda $c_v_l \
+#                             --plot_option_fairness_vs_clusterE $f_v_E \
+#                             --plot_option_convergence $conv
+
+# dataset=Synthetic-unequal
+# cluster_option=kmeans
+# lmd=60.0
+# python test_fair_clustering.py -d $dataset \
+#                              --cluster_option $cluster_option \
+#                              --lmbda-tune $lmd_tune \
+#                              --lmbda $lmd \
+#                              --plot_option_clusters_vs_lambda $c_v_l \
+#                              --plot_option_fairness_vs_clusterE $f_v_E \
+#                              --plot_option_convergence $conv
+
+#Synthetic
+#dataset=Synthetic
+#cluster_option=kmeans
+#lmd=60.0
+#python test_fair_clustering.py -d $dataset \
+#                             --cluster_option $cluster_option \
+#                             --lmbda-tune $lmd_tune \
+#                             --lmbda $lmd \
+#                             --plot_option_clusters_vs_lambda $c_v_l \
+#                             --plot_option_fairness_vs_clusterE $f_v_E \
+#                             --plot_option_convergence $conv
+##Census II
+#dataset=CensusII
+#cluster_option=kmeans
+#lmd=10000
+#python test_fair_clustering.py -d $dataset \
+#                             --cluster_option $cluster_option \
+#                             --lmbda-tune $lmd_tune \
+#                             --lmbda $lmd \
+#                             --plot_option_clusters_vs_lambda $c_v_l \
+#                             --plot_option_fairness_vs_clusterE $f_v_E \
+#                             --plot_option_convergence $conv
+
+#dataset=Adult
+#cluster_option=ncut
+#lmd=10
+#python test_fair_clustering.py -d $dataset \
+#                             --cluster_option $cluster_option \
+#                             --lmbda-tune $lmd_tune \
+#                             --lmbda $lmd \
+#                             --L 1.0 \
+#                             --plot_option_clusters_vs_lambda $c_v_l \
+#                             --plot_option_fairness_vs_clusterE $f_v_E \
+#                             --plot_option_convergence $conv
