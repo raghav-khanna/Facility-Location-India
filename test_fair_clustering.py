fd9c327c1db3e555e3e7f30ca4b21df0634ca9b6
Pranav Chatur <pranavchatur@gmail.com>
2023-10-28 12:14:26 +0530

    Rename variables meaningfully and write comments wherever needed

--- test_fair_clustering.py
+++ test_fair_clustering.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn.preprocessing import scale
 from src.fair_clustering import fair_clustering, km_init
 import src.utils as utils
-from src.dataset_load import read_dataset, dataset_names
+from src.dataset_load import read_dataset
 from src.utils import get_fair_accuracy, get_fair_accuracy_proportional, normalizefea, Logger, str2bool
 from data_visualization import plot_clusters_vs_lambda, plot_fairness_vs_clusterE, plot_convergence, plot_balance_vs_clusterE
 import random
@@ -19,7 +19,7 @@ def main(args):
         np.random.seed(args.seed)
         random.seed(args.seed)
 
-    # Options
+# Set Algorithm Options
     dataset = 'Facility'
     cluster_option = 'kmeans'
     data_dir = osp.join(args.data_dir, dataset)
@@ -27,56 +27,47 @@ def main(args):
     if not osp.exists(data_dir):
         os.makedirs(data_dir)
 
-    # plotting options
+# Set Plotting Options
     plot_option_clusters_vs_lambda = args.plot_option_clusters_vs_lambda
     plot_option_fairness_vs_clusterE = args.plot_option_fairness_vs_clusterE
     plot_option_balance_vs_clusterE = args.plot_option_balance_vs_clusterE
     plot_option_convergence = args.plot_option_convergence
 
-    # ###  Data load
+# Load Dataset
     savepath_compare = osp.join(data_dir, dataset+'.npz')
     if not os.path.exists(savepath_compare):
-        X_org, demograph, K = read_dataset(dataset, data_dir)
-        if X_org.shape[0] > 200000:
-            np.savez_compressed(savepath_compare, X_org=X_org, demograph=demograph, K=K)
+        X_unnormalised, protected_groups, K = read_dataset(data_dir)
+        if X_unnormalised.shape[0] > 200000:
+            np.savez_compressed(savepath_compare, X_unnormalised=X_unnormalised, protected_groups=protected_groups, K=K)
         else:
-            np.savez(savepath_compare, X_org=X_org, demograph=demograph, K=K)
-
+            np.savez(savepath_compare, X_unnormalised=X_unnormalised, protected_groups=protected_groups, K=K)
     else:
         datas = np.load(savepath_compare)
-        X_org = datas['X_org']
-        demograph = datas['demograph']
+        X_unnormalised = datas['X_unnormalised']
+        protected_groups = datas['protected_groups']
         K = datas['K'].item()
 
+# Write to a log file
     log_path = osp.join(data_dir, cluster_option+'_log.txt')
     sys.stdout = Logger(log_path)
-    # Scale and Normalize Features
-    X_org = scale(X_org, axis=0)
-    X = normalizefea(X_org)
 
-    N, D = X.shape
-    print('Cluster number for dataset {} is {}'.format(dataset, K))
-    V_list = [np.array(demograph == j) for j in np.unique(demograph)]
-    V_sum = [x.sum() for x in V_list]
+# Scale and Normalize Features
+    X_unnormalised = scale(X_unnormalised, axis=0)
+    X = normalizefea(X_unnormalised)
+
+# Store and print necessary variables for future use
+    N, Tot_dim = X.shape
+    print('Number of clusters for dataset {} are {}'.format(dataset, K))
+    V = [np.array(protected_groups == j) for j in np.unique(protected_groups)]
+    V_sum = [x.sum() for x in V]
     print('Balance of the dataset {}'.format(min(V_sum)/max(V_sum)))
-
     print('Number of points in the dataset {}'.format(N))
-#    J = len(V_sum)
-
-    # demographic probability for each V_j
-
-    u_V = [x/N for x in V_sum]  # proportional
-    print('Demographic-probabilites: {}'.format(u_V))
+    U = [x/N for x in V_sum]  # proportional
+    print('Demographic-probabilites: {}'.format(U))
     print('Demographic-numbers per group: {}'.format(V_sum))
 
-    #############################################################################
-
-    ######################## Run Fair clustering #################################
-
-    #############################################################################
-    #
+# Initialise Fair Clustering variable
     fairness = True  # Setting False only runs unfair clustering
-
     elapsetimes = []
     avg_balance_set = []
     min_balance_set = []
@@ -86,132 +77,112 @@ def main(args):
     bestacc = 1e10
     best_avg_balance = -1
     best_min_balance = -1
+    labels = None
 
+# Set value for the trade-off controller lambda
     if args.lmbda_tune:
         print('Lambda tune is true')
         lmbdas = np.arange(0, 10000, 100).tolist()
     else:
         lmbdas = [args.lmbda]
-
     length_lmbdas = len(lmbdas)
 
-    l = None
-
+# Obtain initial clusters
     init_C_path = osp.join(data_dir, '{}_init_{}_{}.npz'.format(dataset, cluster_option, K))
     if not osp.exists(init_C_path):
         print('Generating initial seeds')
-        C_init, l_init = km_init(X, K, 'kmeans')
-        np.savez(init_C_path, C_init=C_init, l_init=l_init)
-
+        C_init, labels_init = km_init(X, K, 'kmeans')
+        np.savez(init_C_path, C_init=C_init, labels_init=labels_init)
     else:
         temp = np.load(init_C_path)
-        C_init = temp['C_init']  # Load initial seeds
-        l_init = temp['l_init']
+        C_init = temp['C_init']
+        labels_init = temp['labels_init']
 
+# Repeated iterations of fair_clustering
     for count, lmbda in enumerate(lmbdas):
-
         print('Inside Lambda ', lmbda)
 
-        C, l, elapsed, S, E = fair_clustering(X, K, u_V, V_list, lmbda, args.L, fairness, cluster_option, C_init=C_init, l_init=l_init)
-        print("C - ") 
+    # Calculate clustering measures
+        C, labels, elapsed, S, E = fair_clustering(X, K, U, V, lmbda, args.L, fairness, cluster_option, cluster_init=C_init, labels_init=labels_init)
+        print("C - ")
         print(C.shape)
         print("S - ")
         print(S.shape)
         print("l - ")
-        print(l)
+        print(labels)
 
-        # min_balance, avg_balance = get_fair_accuracy(u_V, V_list, l, N, K)
-        # fairness_error = get_fair_accuracy_proportional(u_V, V_list, l, N, K)
+    # Calculate fairness measures
+        min_balance, avg_balance = get_fair_accuracy(U, V, labels, N, K)
+        fairness_error = get_fair_accuracy_proportional(U, V, labels, N, K)
+        print('lambda = {}, \n fairness_error {: .2f} and \n avg_balance = {: .2f} \n min_balance = {: .2f}'.format(lmbda, fairness_error, avg_balance, min_balance))
 
-        # print('lambda = {}, \n fairness_error {: .2f} and \n avg_balance = {: .2f} \n min_balance = {: .2f}'.format(lmbda, fairness_error, avg_balance, min_balance))
+    # Assign (and print) new values to global values (if conditions match)
+        if avg_balance > best_avg_balance:
+            best_avg_balance = avg_balance
+            best_lambda_avg_balance = lmbda
+        if min_balance > best_min_balance:
+            best_min_balance = min_balance
+            best_lambda_min_balance = lmbda
+        if fairness_error < bestacc:
+            bestacc = fairness_error
+            best_lambda_acc = lmbda
+        print('Best fairness_error %0.4f' % bestacc, '|Error lambda = ', best_lambda_acc)
+        print('Best  Avg balance %0.4f' % best_avg_balance, '| Avg Balance lambda = ', best_lambda_avg_balance)
+        print('Best  Min balance %0.4f' % best_min_balance, '| Min Balance lambda = ', best_lambda_min_balance)
+        print(float(E['fair_cluster_E'][0]))
 
-        # Plot the figure with clusters
+    # Plot results of the iteration
+        if plot_option_convergence is True and count == 0:
+            filename = osp.join(output_path, 'Fair_{}_convergence_{}.png'.format(cluster_option, dataset))
+            E_fair = E['fair_cluster_E']
+            plot_convergence(cluster_option, filename, E_fair)
 
-    #     if dataset in ['Synthetic', 'Synthetic-unequal'] and plot_option_clusters_vs_lambda == True:
-    #         cluster_plot_location = osp.join(output_path, 'cluster_output')
-    #         if not osp.exists(cluster_plot_location):
-    #             os.makedirs(cluster_plot_location)
+    # Add results to global arrays (for later reference?)
+        elapsetimes.append(elapsed)
+        avg_balance_set.append(avg_balance)
+        min_balance_set.append(min_balance)
+        fairness_error_set.append(fairness_error)
+        E_cluster_set.append(E['cluster_E'][-1])
+        E_cluster_discrete_set.append(E['cluster_E_discrete'][-1])
 
-    #         filename = osp.join(cluster_plot_location, 'cluster-plot_fair_{}-{}_lambda_{}.png'.format(cluster_option, dataset, lmbda))
-    #         plot_clusters_vs_lambda(X_org, l, filename, dataset, lmbda, fairness_error)
+# Calculate elapsed time used by loop
+    avgelapsed = sum(elapsetimes)/len(elapsetimes)
+    print('avg elapsed ', avgelapsed)
 
-    #     if avg_balance > best_avg_balance:
-    #         best_avg_balance = avg_balance
-    #         best_lambda_avg_balance = lmbda
-
-    #     if min_balance > best_min_balance:
-    #         best_min_balance = min_balance
-    #         best_lambda_min_balance = lmbda
-
-    #     if fairness_error < bestacc:
-    #         bestacc = fairness_error
-    #         best_lambda_acc = lmbda
-
-    #     if plot_option_convergence == True and count == 0:
-
-    #         filename = osp.join(output_path, 'Fair_{}_convergence_{}.png'.format(cluster_option, dataset))
-    #         E_fair = E['fair_cluster_E']
-    #         plot_convergence(cluster_option, filename, E_fair)
-
-    #     # print('Best fairness_error %0.4f' % bestacc, '|Error lambda = ', best_lambda_acc)
-    #     # print('Best  Avg balance %0.4f' % best_avg_balance, '| Avg Balance lambda = ', best_lambda_avg_balance)
-    #     # print('Best  Min balance %0.4f' % best_min_balance, '| Min Balance lambda = ', best_lambda_min_balance)
-    #     elapsetimes.append(elapsed)
-    #     avg_balance_set.append(avg_balance)
-    #     min_balance_set.append(min_balance)
-    #     fairness_error_set.append(fairness_error)
-    #     E_cluster_set.append(E['cluster_E'][-1])
-    #     E_cluster_discrete_set.append(E['cluster_E_discrete'][-1])
-
-    # # print("--------------------------------------->>>>>>>> E_Cluster - ")
-    # # print(float(E['fair_cluster_E'][0]))
-
-    # avgelapsed = sum(elapsetimes)/len(elapsetimes)
-    # # print('avg elapsed ', avgelapsed)
-
-    # if plot_option_fairness_vs_clusterE == True and length_lmbdas > 1:
-
-    #     savefile = osp.join(data_dir, 'Fair_{}_fairness_vs_clusterEdiscrete_{}.npz'.format(cluster_option, dataset))
-    #     filename = osp.join(output_path, 'Fair_{}_fairness_vs_clusterEdiscrete_{}.png'.format(cluster_option, dataset))
-    #     plot_fairness_vs_clusterE(cluster_option, savefile, filename, lmbdas, fairness_error_set, min_balance_set, avg_balance_set, E_cluster_discrete_set)
-
-    # if plot_option_balance_vs_clusterE == True and length_lmbdas > 1:
-
-    #     savefile = osp.join(data_dir, 'Fair_{}_balance_vs_clusterEdiscrete_{}.npz'.format(cluster_option, dataset))
-    #     filename = osp.join(output_path, 'Fair_{}_balance_vs_clusterEdiscrete_{}.png'.format(cluster_option, dataset))
-
-    #     plot_balance_vs_clusterE(cluster_option, savefile, filename, lmbdas, fairness_error_set, min_balance_set, avg_balance_set, E_cluster_discrete_set)
+# Add few more plots if cluster options have them
+    if plot_option_fairness_vs_clusterE is True and length_lmbdas > 1:
+        savefile = osp.join(data_dir, 'Fair_{}_fairness_vs_clusterEdiscrete_{}.npz'.format(cluster_option, dataset))
+        filename = osp.join(output_path, 'Fair_{}_fairness_vs_clusterEdiscrete_{}.png'.format(cluster_option, dataset))
+        plot_fairness_vs_clusterE(cluster_option, savefile, filename, lmbdas, fairness_error_set, min_balance_set, avg_balance_set, E_cluster_discrete_set)
+    if plot_option_balance_vs_clusterE is True and length_lmbdas > 1:
+        savefile = osp.join(data_dir, 'Fair_{}_balance_vs_clusterEdiscrete_{}.npz'.format(cluster_option, dataset))
+        filename = osp.join(output_path, 'Fair_{}_balance_vs_clusterEdiscrete_{}.png'.format(cluster_option, dataset))
+        plot_balance_vs_clusterE(cluster_option, savefile, filename, lmbdas, fairness_error_set, min_balance_set, avg_balance_set, E_cluster_discrete_set)
 
 
 if __name__ == '__main__':
-    # print("------------------------------------->>>>>")
     parser = argparse.ArgumentParser(description="Clustering with Fairness Constraints")
+
+# Add clustering options
     parser.add_argument('--seed', type=int, default=None)
-    # dataset
-    parser.add_argument('-d', '--dataset', type=str, default='Facility',
-                        choices=dataset_names())
-    # clustering method
+    parser.add_argument('-d', '--dataset', type=str, default='Facility')
     parser.add_argument('--cluster_option', type=str, default='kmeans')
 
-    # Plot options
-    parser.add_argument('--plot_option_clusters_vs_lambda', default=False, type=str2bool,
-                        help="plot clusters in 2D w.r.t lambda")
-    parser.add_argument('--plot_option_fairness_vs_clusterE', default=False, type=str2bool,
-                        help="plot clustering original energy w.r.t fairness")
-    parser.add_argument('--plot_option_balance_vs_clusterE', default=False, type=str2bool,
-                        help="plot clustering original energy w.r.t balance")
-    parser.add_argument('--plot_option_convergence', default=False, type=str2bool,
-                        help="plot convergence of the fair clustering energy")
+# Add Plot options
+    parser.add_argument('--plot_option_clusters_vs_lambda', default=False, type=str2bool, help="plot clusters in 2D w.r.t lambda")
+    parser.add_argument('--plot_option_fairness_vs_clusterE', default=False, type=str2bool, help="plot clustering original energy w.r.t fairness")
+    parser.add_argument('--plot_option_balance_vs_clusterE', default=False, type=str2bool, help="plot clustering original energy w.r.t balance")
+    parser.add_argument('--plot_option_convergence', default=False, type=str2bool, help="plot convergence of the fair clustering energy")
 
-    # Lambda
+# Add Lambda options
     parser.add_argument('--lmbda', type=float, default=50)  # specified lambda
     parser.add_argument('--lmbda-tune', type=str2bool, default=True)  # run in a range of different lambdas
     parser.add_argument('--L', type=float, default=2.0)  # Lipchitz value in bound update
 
-    # misc
+# Save results to directory
     working_dir = osp.dirname(osp.abspath(__file__))
-    parser.add_argument('--data_dir', type=str, metavar='PATH',
-                        default=osp.join(working_dir, 'data'))
-    parser.add_argument('--output_path', type=str, metavar='PATH',
-                        default=osp.join(working_dir, 'outputs'))
+    parser.add_argument('--data_dir', type=str, metavar='PATH', default=osp.join(working_dir, 'data'))
+    parser.add_argument('--output_path', type=str, metavar='PATH', default=osp.join(working_dir, 'outputs'))
+
+# Start code
     main(parser.parse_args())
