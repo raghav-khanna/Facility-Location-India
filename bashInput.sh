47b44f78a727cf7fc19af55aa5325fdaf5ce44aa
Pranav Chatur <pranavchatur@gmail.com>
2023-11-01 17:47:58 +0530

    Update bash_input.sh

--- bashInput.sh
+++ bashInput.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-facilities=(1 2)
-
-for facility in "${facilities[@]}"; do
+for facility in {1..641..1}; do
+	echo "Working on $facility"
     py kmeanspp.py -f "$facility" > "data/results/$facility-output.txt"
 done
